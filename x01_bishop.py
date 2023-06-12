01a900bc8c65398f275433253efe7af755a07ccf
ColeCollector <colen827@deltalearns.ca>
2023-06-12 11:35:08 -0700

    a

--- x01_bishop.py
+++ x01_bishop.py
@@ -9,8 +9,35 @@ def bishop(square):
   return:
   list of possible squares that the bishop can move to:
   """
+  print(square)
+  available = []
+
+  crow = square[0]
+  ccollumn = square[1]
   
-  return None
+  row = ['a','b','c','d','e','f','g','h']
+  collumn = ['1','2','3','4','5','6','7','8']
+  
+  rownum = row.index(crow)
+  collumnum = collumn.index(ccollumn)
+  
+  for i in range(0,8):
+    x = row[i]
+    differ = abs(rownum - row.index(x))
+
+    if collumnum-differ+1 > 0 and collumnum-differ+1 <= 8:
+      available.append(f"{x}{collumnum-differ+1}")
+      
+    if collumnum+differ+1 <= 8 and collumnum+differ+1 <= 8:
+      available.append(f"{x}{collumnum+differ+1}")
+
+    if str(square) in available:
+      available.remove(str(square))
+  
+  print(available)
+
+    
+  return available
 
 
 def main():
@@ -19,7 +46,7 @@ def main():
   assert myList == ['a8', 'b7', 'c6', 'd1', 'd5', 'e2', 'e4', 'g2', 'g4', 'h1', 'h5']
   myList = bishop('g7')
   myList.sort()
-  assert myList = ['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'f8', 'h6', 'h8']
+  assert myList == ['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'f8', 'h6', 'h8']
 
 if __name__ == "__main__":
   main()
